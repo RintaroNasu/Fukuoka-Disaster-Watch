ef95cfdbd70bd97a32c36310e2a585736bfde6ef
RintaroNasu <rinta022823@gmail.com>
2024-11-16 02:08:48 +0900

    fix:headerのCSS

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,13 @@
+"use client";
 
+import dynamic from "next/dynamic";
 
 export default function Home() {
-  return <div>test</div>;
+  const LeafletMap = dynamic(() => import("../components/LeafletMap").then((mod) => mod.LeafletMap), { ssr: false });
+  return (
+    <>
+      <div className="flex justify-center text-3xl font-semibold bg-[#0f1b2a] text-white p-4">福岡災害予測アプリ</div>
+      <LeafletMap />
+    </>
+  );
 }
