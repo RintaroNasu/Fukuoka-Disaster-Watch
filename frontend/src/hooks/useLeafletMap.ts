2615d8ea8e1a2782d78aca0981e01face5ac9801
RintaroNasu <rinta022823@gmail.com>
2024-11-18 20:38:31 +0900

    feat:tsunamiデータをfetch

--- frontend/src/hooks/useLeafletMap.ts
+++ frontend/src/hooks/useLeafletMap.ts
@@ -1,11 +1,12 @@
 "use client";
-
 import { useEffect, useRef } from "react";
+
+import { Coordinate, TsunamiData } from "@/utils/type";
+
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
-import { Coordinate } from "@/utils/type";
 
-const useLeafletMap = (center: Coordinate) => {
+export const useLeafletMap = (center: Coordinate, tsunami: TsunamiData) => {
   const mapRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -16,13 +17,16 @@ const useLeafletMap = (center: Coordinate) => {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
+      tsunami.forEach((feature) => {
+        const coordinates = (feature.geometry.coordinates[0][0] as number[][]).map((coord) => [coord[1], coord[0]]);
+        L.polygon(coordinates as L.LatLngExpression[], { color: "blue" }).addTo(map);
+      });
+
       return () => {
         map.remove();
       };
     }
-  }, [center]);
+  }, [center, tsunami]);
 
   return mapRef;
 };
-
-export { useLeafletMap };
