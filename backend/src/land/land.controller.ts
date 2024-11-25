5d03341848b3693f32b1017e4abca6ff8b555066
RintaroNasu <rinta022823@gmail.com>
2024-11-25 14:31:42 +0900

    feat:検索機能の実装+tsunamiコントローラーの削除

--- backend/src/land/land.controller.ts
+++ backend/src/land/land.controller.ts
@@ -1,10 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { LandService } from './land.service';
 @Controller('land')
 export class LandController {
   constructor(private readonly LandService: LandService) {}
-  @Get()
-  async getLand() {
-    return await this.LandService.getLand();
+  @Post()
+  async getLand(@Body('searchCities') searchCities: string[]) {
+    return await this.LandService.getLand(searchCities);
   }
 }
