6e9b06d0d4aa1cbf693f9e6afbbada1eb05c9eae
RintaroNasu <rinta022823@gmail.com>
2024-11-25 12:47:44 +0900

    fix:津波データ削除

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,11 @@
 "use client";
 
 import { SearchConditionModal } from "@/components/SearchConditionModal";
-import { LandData, TsunamiData } from "@/utils/type";
+import { LandData } from "@/utils/type";
 import dynamic from "next/dynamic";
 import { useState } from "react";
 
 export default function Home() {
-  const [tsunami, setTsunami] = useState<TsunamiData>([]);
   const [land, setLand] = useState<LandData>([]);
 
   const LeafletMap = dynamic(() => import("../components/LeafletMap").then((mod) => mod.LeafletMap), { ssr: false });
@@ -16,9 +15,9 @@ export default function Home() {
       <div className="relative">
         <div className="fixed top-0 right-0 w-full z-[3000] flex justify-center text-3xl font-semibold bg-[#0f1b2a] text-white p-4">福岡災害予測アプリ</div>
         <div className="mt-16">
-          <LeafletMap tsunami={tsunami} land={land} />
+          <LeafletMap land={land} />
         </div>
-        <SearchConditionModal setTsunami={setTsunami} setLand={setLand}/>
+        <SearchConditionModal setLand={setLand} />
       </div>
     </>
   );
--- frontend/src/components/LeafletMap.tsx
+++ frontend/src/components/LeafletMap.tsx
@@ -1,22 +1,17 @@
 "use client";
 
-import { useEffect, useState } from "react";
-
-import { getTsunami } from "@/utils/getTsunami";
-import { getLand } from "@/utils/getLand";
-import { LandData, TsunamiData } from "@/utils/type";
+import { LandData } from "@/utils/type";
 
 import { useLeafletMap } from "@/hooks/useLeafletMap";
 
 import "leaflet/dist/leaflet.css";
 
 type Props = {
-  tsunami: TsunamiData;
   land: LandData;
 };
 
 export const LeafletMap = (props: Props) => {
-  const mapRef = useLeafletMap([33.5902, 130.4207], props.tsunami, props.land);
+  const mapRef = useLeafletMap([33.5902, 130.4207], props.land);
 
   return (
     <>
