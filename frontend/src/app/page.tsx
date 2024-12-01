d1c91d0b47521c4d4e36bcd09f44b341d98387cb
RintaroNasu <rinta022823@gmail.com>
2024-12-01 20:38:38 +0900

    feat:auth周りのボタンをheaderに追加

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,19 +1,45 @@
 "use client";
 
 import { SearchConditionModal } from "@/components/SearchConditionModal";
+import { successToast } from "@/utils/toast";
 import { LandData } from "@/utils/type";
 import dynamic from "next/dynamic";
-import { useState } from "react";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   const [land, setLand] = useState<LandData>([]);
 
   const LeafletMap = dynamic(() => import("../components/LeafletMap").then((mod) => mod.LeafletMap), { ssr: false });
 
+  useEffect(() => {
+    const token = localStorage.getItem("access_token");
+    setIsLoggedIn(!!token);
+  }, []);
+
+  const onClickLogout = () => {
+    localStorage.removeItem("access_token");
+    successToast("ログアウトしました。");
+    setIsLoggedIn(false);
+  };
+
   return (
     <>
       <div className="relative">
-        <div className="fixed top-0 right-0 w-full z-[3000] flex justify-center text-3xl font-semibold bg-[#0f1b2a] text-white p-4">福岡災害予測アプリ</div>
+        <div className="fixed top-0 right-0 w-full z-[3000] flex justify-between bg-[#0f1b2a] text-white p-4">
+          <div className="text-3xl font-semibold">Fukuoka Disaster Watch</div>
+          <div className="flex gap-5 items-center font-semibold">
+            {!isLoggedIn && (
+              <>
+                <Link href="/sign_in">ログイン</Link>
+                <Link href="/sign_up">新規登録</Link>
+              </>
+            )}
+            {isLoggedIn && <button onClick={onClickLogout}>ログアウト</button>}
+          </div>
+        </div>
         <div className="mt-16">
           <LeafletMap land={land} />
         </div>
