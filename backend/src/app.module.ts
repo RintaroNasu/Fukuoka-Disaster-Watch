cc551301d5e03bf190c1aefc2f9fa063e5174ee4
RintaroNasu <rinta022823@gmail.com>
2024-12-01 20:40:42 +0900

    auth周りをimport

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -8,10 +8,18 @@ import { LandModule } from './land/land.module';
 import { AiAnalysisController } from './ai-analysis/ai-analysis.controller';
 import { AiAnalysisService } from './ai-analysis/ai-analysis.service';
 import { AiAnalysisModule } from './ai-analysis/ai-analysis.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
+import { AuthService } from './auth/auth.service';
 
 @Module({
-  imports: [PrismaModule, LandModule, AiAnalysisModule],
-  controllers: [AppController, LandController, AiAnalysisController],
-  providers: [AppService, LandService, AiAnalysisService],
+  imports: [PrismaModule, LandModule, AiAnalysisModule, AuthModule],
+  controllers: [
+    AppController,
+    LandController,
+    AiAnalysisController,
+    AuthController,
+  ],
+  providers: [AppService, LandService, AiAnalysisService, AuthService],
 })
 export class AppModule {}
