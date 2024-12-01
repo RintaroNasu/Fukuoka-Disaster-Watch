7cf1139332fd929635b174bf49dd467a502dc003
RintaroNasu <rinta022823@gmail.com>
2024-12-01 20:33:21 +0900

    refactor:ボタンをコンポーネント化

---
+++ frontend/src/components/parts/PrimaryButton.tsx
@@ -0,0 +1,20 @@
+type Props = {
+  className?: string;
+  children: React.ReactNode;
+  disabled?: boolean;
+  onClick?: () => void;
+};
+
+export const PrimaryButton = (props: Props) => {
+  return (
+    <button
+      onClick={props.onClick}
+      disabled={props.disabled}
+      className={`text-white rounded-2xl px-4 py-2 bg-blue-500  
+        ${props.disabled ? "cursor-not-allowed bg-blue-400" : "hover:bg-blue-700"} 
+        ${props.className}`}
+    >
+      {props.children}
+    </button>
+  );
+};
