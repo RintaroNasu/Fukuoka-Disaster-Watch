f9a9b017dde4911fc4ac8d3613698a202cc14902
RintaroNasu <rinta022823@gmail.com>
2024-12-03 11:18:27 +0900

    feature/登録した全てのユーザーにメール送信

--- backend/src/comments/comments.service.ts
+++ backend/src/comments/comments.service.ts
@@ -1,15 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'prisma/prisma.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
+import { sendMail } from 'src/utils/mailer';
 
 @Injectable()
 export class CommentsService {
   constructor(private readonly prisma: PrismaService) {}
 
   async createComment(data: CreateCommentDto) {
-    return this.prisma.comment.create({
+    const comment = await this.prisma.comment.create({
       data,
     });
+
+    await this.sendNotificationToUsers(comment);
+    return comment;
+  }
+
+  private async sendNotificationToUsers(comment) {
+    const users = await this.prisma.user.findMany({
+      select: { email: true },
+    });
+
+    const subject = 'コメントが投稿されました。すぐに確認しましょう。';
+    const text = `${comment.content}`;
+
+    for (const user of users) {
+      await sendMail(user.email, subject, text);
+    }
   }
 
   async getComments() {
