038edb40aa3ee17535135329ecc675bec187a221
RintaroNasu <rinta022823@gmail.com>
2024-12-03 11:13:27 +0900

    feat:送信機能実装

---
+++ backend/src/utils/mailer.ts
@@ -0,0 +1,26 @@
+import * as nodemailer from 'nodemailer';
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+  secure: true,
+});
+
+export const sendMail = async (to: string, subject: string, text: string) => {
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to,
+    subject,
+    text,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log('メール送信成功');
+  } catch (error) {
+    console.error('メール送信エラー:', error);
+  }
+};
