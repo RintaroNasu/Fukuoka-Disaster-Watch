6a75407047fd7c09af95d28c5be052fac23102d8
RintaroNasu <rinta022823@gmail.com>
2024-12-10 01:07:56 +0900

    fix:避難所表示ボタン追加

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,23 +2,33 @@
 
 import { Header } from "@/components/Header";
 import { SearchConditionModal } from "@/components/SearchConditionModal";
-import { LandData } from "@/utils/type";
+import { getShelter } from "@/utils/api/getShelter";
+import { LandData, ShelterData } from "@/utils/type";
 import dynamic from "next/dynamic";
 import { useState } from "react";
 
 export default function Home() {
   const [land, setLand] = useState<LandData>([]);
+  const [shelter, setShelter] = useState<ShelterData>([]);
 
   const LeafletMap = dynamic(() => import("../components/LeafletMap").then((mod) => mod.LeafletMap), { ssr: false });
+
+  const onClickDisplayShelter = async () => {
+    const json = await getShelter();
+    setShelter(json);
+  };
 
   return (
     <>
       <div className="relative">
         <Header />
         <div className="mt-16">
-          <LeafletMap land={land} />
+          <LeafletMap land={land} shelter={shelter} />
         </div>
         <SearchConditionModal setLand={setLand} />
+        <button className="hover:bg-gray-500 fixed gap-2 right-14 z-[400] rounded-2xl top-40 bg-gray-900 shadow-xl text-white flex items-center px-4 py-3" onClick={onClickDisplayShelter}>
+          避難所表示
+        </button>
       </div>
     </>
   );
